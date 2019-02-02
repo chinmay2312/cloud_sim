7960b6e65d979b8bb7857e96d1fb566cf1f79c44
Chinmay Gangal <cganga2@uic.edu>
2019-02-01 23:35:26 -0600

    sample unit test

--- src/test/scala/myCloudSimTest.scala
+++ src/test/scala/myCloudSimTest.scala
@@ -4,7 +4,15 @@ import org.scalatest.FlatSpec
 class myCloudSimTest extends FlatSpec {
   //test("myCloudSim.")
 
-  "createBroker() method"
+  behavior of "createBroker() method"
+
+  it should "return a new object of type DatacenterBroker" in {
+    def f[T](v: T) = v
+
+    assert(f(myCloudSim.createBroker()) == DatacenterBroker)
+  }
+
+  /*"createBroker() method"
 
   should
 
@@ -15,5 +23,5 @@ class myCloudSimTest extends FlatSpec {
     def f[T](v: T) = v
 
     assert(f(broker) == DatacenterBroker)
-  }
+  }*/
 }
